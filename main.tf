2d4dbe06fac7b8bb57022c7ddf10c239b843005b
iamdevleper <59489492+iamdevleper@users.noreply.github.com>
2020-10-27 13:09:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,1 @@
-module "virtual-machine" {
-  source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
-  version = "0.1.2"
 
-  app_name    = "ninjas-skunkworks-nginx"
-  description = "a skunkworks project"
-  environment = var.environment
-  owner       = "008103"
-
-  num_cpus  = "2"
-  memory    = "2048"
-  disk_size = "20"
-}
-
-variable "environment" {}
-
-output "name" { value = module.virtual-machine.name }
-output "ssh_ip" { value = module.virtual-machine.ssh_addr }
