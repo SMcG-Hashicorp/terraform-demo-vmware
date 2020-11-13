72b38436bccf7d6568d96feb82198d1b24ec9b99
assareh <assareh@hashicorp.com>
2020-11-13 11:25:02 -0800

    added a vm

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "virtual-machine" {
+  source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
+  version = "0.2.1"
+
+  app_name    = "ninjas-skunkworks-nginx"
+  description = "a skunkworks project"
+  environment = var.environment
+  owner       = "008103"
+}
+
+variable "environment" {}
+
+output "name" { value = module.virtual-machine.name }
+output "http_ip" { value = module.virtual-machine.http_addr }
+output "ssh_ip" { value = module.virtual-machine.ssh_addr }
