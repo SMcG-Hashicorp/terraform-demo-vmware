20f3e9dec0e6e32e90e30ce0996f06f14bccc784
assareh <assareh@hashicorp.com>
2021-01-25 12:43:16 -0800

    baseline config with outputs

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-variable "app_name" {}
-variable "description" {}
-variable "environment" {}
-variable "owner" {}
-variable "vsphere_server" {}
-variable "vsphere_dc" {}
-variable "vsphere_host" {}
-variable "vsphere_datastore" {}
-variable "vsphere_network" {}
-
 module "virtual-machine" {
   source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
   version = "1.0.1"
@@ -17,11 +7,11 @@ module "virtual-machine" {
   environment = var.environment
   owner       = var.owner
 
-  vsphere_server = var.vsphere_server
-  vsphere_dc = var.vsphere_dc
-  vsphere_host = var.vsphere_host
+  vsphere_server    = var.vsphere_server
+  vsphere_dc        = var.vsphere_dc
+  vsphere_host      = var.vsphere_host
   vsphere_datastore = var.vsphere_datastore
-  vsphere_network = var.vsphere_network
+  vsphere_network   = var.vsphere_network
 
   # num_cpus  = "2"
   # memory    = "2048"
