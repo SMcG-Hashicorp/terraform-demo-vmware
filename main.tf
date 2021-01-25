cfa3ab0c0749d00d6082cc2ef1e031b15af8cd7e
assareh <assareh@hashicorp.com>
2021-01-25 12:27:35 -0800

    baseline config

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
+variable "app_name" {}
+variable "description" {}
 variable "environment" {}
+variable "owner" {}
+variable "vsphere_server" {}
+variable "vsphere_dc" {}
+variable "vsphere_host" {}
+variable "vsphere_datastore" {}
+variable "vsphere_network" {}
 
 module "virtual-machine" {
   source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
   version = "1.0.1"
 
-  app_name    = "ninjas-skunkworks-nginx"
-  description = "a skunkworks project"
+  app_name    = var.app_name
+  description = var.description
   environment = var.environment
-  owner       = "008103"
+  owner       = var.owner
+
+  vsphere_server = var.vsphere_server
+  vsphere_dc = var.vsphere_dc
+  vsphere_host = var.vsphere_host
+  vsphere_datastore = var.vsphere_datastore
+  vsphere_network = var.vsphere_network
 
   # num_cpus  = "2"
   # memory    = "2048"
