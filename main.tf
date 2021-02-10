c7fbcd10708ae19e7af320c9f2b16abee3dfd595
Andy Assareh <assareh@hashicorp.com>
2021-02-09 21:57:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,13 +17,3 @@ variable "environment" {}
 output "name" { value = module.virtual-machine.name }
 output "http_ip" { value = module.virtual-machine.http_addr }
 output "ssh_ip" { value = module.virtual-machine.ssh_addr }
-
-  provider "vsphere" {
-  allow_unverified_ssl = true
-}
-
-# use for Edinburgh? - need to test
-resource "vsphere_resource_pool" "resource_pool" {
-  name                    = "terraform-resource-pool-test"
-  parent_resource_pool_id = module.virtual-machine.resource_pool_id
-}
