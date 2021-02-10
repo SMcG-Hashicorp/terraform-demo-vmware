d1e0e80968283f3fb5b56d2ff2c48135a75438a2
Andy Assareh <assareh@hashicorp.com>
2021-02-09 22:07:19 -0800

    blank

--- main.tf
+++ main.tf
@@ -1,19 +1,1 @@
-module "virtual-machine" {
-  source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
-  version = "1.0.1"
 
-  app_name    = "ninjas-skunkworks-nginx"
-  description = "a skunkworks project"
-  environment = var.environment
-  owner       = "008103"
-
-  # num_cpus  = "4" 
-  memory    = "10240"
-  # disk_size = "20"
-}
-
-variable "environment" {}
-
-output "name" { value = module.virtual-machine.name }
-output "http_ip" { value = module.virtual-machine.http_addr }
-output "ssh_ip" { value = module.virtual-machine.ssh_addr }
