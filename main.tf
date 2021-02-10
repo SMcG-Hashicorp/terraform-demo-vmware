4803412078d753c9ab02e4e3e4d0bf5978d1a5ed
Andy Assareh <assareh@hashicorp.com>
2021-02-10 13:52:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+module "virtual-machine" {
+  source  = "app.terraform.io/hashidemos/virtual-machine/vsphere"
+  version = "1.0.1"
 
+  app_name    = "ninjas-skunkworks-nginx"
+  description = "a skunkworks project"
+  environment = var.environment
+  owner       = "008103"
+
+  # num_cpus  = "4"
+  # memory    = "8192"
+  # disk_size = "100"
+}
+
+variable "environment" {}
+
+output "name" { value = module.virtual-machine.name }
+output "http_ip" { value = module.virtual-machine.http_addr }
+output "ssh_ip" { value = module.virtual-machine.ssh_addr }
